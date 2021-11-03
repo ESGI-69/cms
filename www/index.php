5fdab2e2dcaedc75fc14af61cf29a23ced7ccae0
Timdev0 <quentin.peltier.pro@gmail.com>
2021-11-03 17:36:41 +0100

    feat(index.php): retrieve routeName from url

--- www/index.php
+++ www/index.php
@@ -1,3 +1,13 @@
 <?php
 
+// split parameters and put them in an array
+$params = explode('/', $_GET['p']);
 
+// if 1 parameters exist 
+if ($params[0] != "") {
+  // save first parameter in $routeName
+  $routeName = '/'.$params[0];
+//else return  '/' in $routeName
+} else {
+  $routeName = '/';
+}
