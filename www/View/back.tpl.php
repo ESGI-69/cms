80dfa762ae6ba4ab412e233017daf06c644df59f
Timdev0 <quentin.peltier.pro@gmail.com>
2022-02-03 14:51:19 +0100

    feat(chartjs-implementation): Chart.js implementation + view management with front et back templates

--- www/View/back.tpl.php
+++ www/View/back.tpl.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Template BACK</title>
     <meta name="description" content="Description de ma page">
+    <script src="/js/chart.min.js"></script>
 </head>
     <body>
         <?php include "View/".$this->view.".view.php"; ?>
