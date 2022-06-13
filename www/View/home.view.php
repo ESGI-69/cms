c65f2fcb66bf626befffdc13b987eff27fed3424
Mr_Pink_Cat <gatien.anizan@gmail.com>
2022-06-13 16:01:45 +0200

    feat(view): add auth management to views

--- www/View/home.view.php
+++ www/View/home.view.php
@@ -1,1 +1,10 @@
 <h1>Page d'accueil</h1>
+
+<?php if ($isAuth) : ?>
+  <p>Bienvenue <?= $userInfos["firstname"] ?></p>
+  <a href="./logout">Se déconnecter</a>
+<?php else : ?>
+  <a href="./login">Se connecter</a>
+  <a href="./register">S'inscrire</a>
+<?php endif; ?>
+<br>
