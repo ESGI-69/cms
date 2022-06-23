fe3df4244ee57c6c4f9cfcf9c782205aecbdde96
Mr_Pink_Cat <gatien.anizan@gmail.com>
2022-06-23 19:48:07 +0200

    fix(media): change style

--- www/View/mediasList.view.php
+++ www/View/mediasList.view.php
@@ -1,7 +1,12 @@
-<a href="/media-manager">Ajouter</a>
-
-<?php foreach ($medias as $media) : ?>
-  <img src="<?= $media["path"] ?>" alt="<?= $media["name"] ?>">
-  <a href="/media-manager?id=<?= $media["id"] ?>" class="button">Editer</a>
-  <a href="/medias-list?deletedId=<?= $media["id"] ?>" class="button">Supprimer</a>
-<?php endforeach; ?>
+<div class="action-buttons">
+  <a class="button button--primary button--big" href="/article-manager">Ajouter un Média</a>
+</div>
+<div class="media-list">
+  <?php foreach ($medias as $media) : ?>
+    <div class="media-list__item">
+      <img src="<?= $media["path"] ?>" alt="<?= $media["name"] ?>">
+      <a href="/media-manager?id=<?= $media["id"] ?>" class="button">Editer</a>
+      <a href="/medias-list?deletedId=<?= $media["id"] ?>" class="button">Supprimer</a>
+    </div>
+  <?php endforeach; ?>
+</div>
