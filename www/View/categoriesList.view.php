b20360b5fd5925eb19afeb1e34184e8d15791b47
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-06-27 18:41:29 +0200

    feat(35-add-category): add and remove

---
+++ www/View/categoriesList.view.php
@@ -0,0 +1,14 @@
+<div class="action-buttons">
+  <a class="button button--primary button--big" href="/category-manager">New category</a>
+</div>
+<div class="category-list">
+  <?php foreach ($categories as $category) : ?>
+    <div class="category-list__item">
+      <span class="category-list__item__name">
+        <?= $category["name"] ?>
+      </span>
+      <a href="/category-manager?id=<?= $category["id"] ?>" class="button button--primary">Editer</a>
+      <a href="/categories-list?deletedId=<?= $category["id"] ?>" class="button button--danger">Supprimer</a>
+    </div>
+  <?php endforeach; ?>
+</div>
