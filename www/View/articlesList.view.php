da10bf3ec28520d3064b6b5c733134b4bc369946
Mr_Pink_Cat <gatien.anizan@gmail.com>
2022-07-10 17:13:13 +0200

    feat(article list): add list page

--- www/View/articlesList.view.php
+++ www/View/articlesList.view.php
@@ -1,3 +1,11 @@
 <div class="action-buttons">
   <a class="button button--primary button--big" href="/article-manager">New article</a>
 </div>
+
+<?php foreach ($articles as $article) : ?>
+  <div>
+    <p><?= $article->title ?></p>
+    <a href="/article-manager?id=<?= $article->id ?>" class="button button--primary">Editer</a>
+    <a href="/articles-list?deletedId=<?= $article->id ?>" class="button button--danger">Supprimer</a>
+  </div>
+<?php endforeach; ?>
