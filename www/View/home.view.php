aeee45cf80c60aeeca0a6ecec11d4ff7bb12378e
Mr_Pink_Cat <gatien.anizan@gmail.com>
2022-07-11 22:51:39 +0200

    feat(home): show the last post on homepage

--- www/View/home.view.php
+++ www/View/home.view.php
@@ -4,3 +4,20 @@
   <input class="input" type="text" name="search" placeholder="Rechercher un article" />
   <button class="button button--primary button--big" type="submit">Search</button>
 </form>
+<span class="last-article">
+  <h2 class="last-article__title">Recent posts</h2>
+  <div class="last-article__list">
+    <?php foreach ($articles as $article) : ?>
+      <div class="last-article__list__item">
+        <img class="last-article__list__item__img" src="<?= $article->media_id ?>" alt="<?= $article->title ?>" />
+        <a href="/article?id=<?= $article->id ?>" class="last-article__list__item__link">
+          <h3><?= $article->title ?></h3>
+        </a>
+        <span class="last-article__list__item__date"><?= $article->createdAt ?></span>
+        <!-- Author -->
+        <span class="last-article__list__item__author"><?= $article->user_id ?></span>
+        <p class="last-article__list__item__content"><?= $article->content ?></p>
+      </div>
+    <?php endforeach; ?>
+  </div>
+</span>
