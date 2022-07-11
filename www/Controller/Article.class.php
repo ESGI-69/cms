5899c12e1d8d067b800cf5f371fc03d6d407b4b7
Mr_Pink_Cat <gatien.anizan@gmail.com>
2022-07-11 22:07:19 +0200

    feat(articleEdit): add the edit feature

--- www/Controller/Article.class.php
+++ www/Controller/Article.class.php
@@ -29,23 +29,41 @@ class Article
     $success = false;
     $article = new ArticleModel();
     $formErrors = [];
+    $registerError = false;
 
-    if (!empty($_POST)) {
-      $formErrors = Verificator::checkForm($article->getForm(), $_POST);
-      if (count($formErrors) === 0) {
-        $article->setArticleInfo();
-        $registerError = $article->checkExisting('title');
-        if ($registerError === false) {
-          $article->save();
-          $success = true;
-          header("Location: /articles-list");
-        } else {
-          $formErrors[] = "Titre déjà utilisé";
+    if (isset($_GET['id'])) {
+      $article->getArticleInfo($_GET['id']);
+      if (!empty($_POST)) {
+        $formErrors = Verificator::checkForm($article->getForm(), $_POST);
+        if (count($formErrors) === 0) {
+          $article->setArticleInfo();
+          if ($registerError === false) {
+            $article->edit();
+            $success = true;
+            header("Location: /articles-list");
+          } else {
+            $formErrors[] = "Titre déjà utilisé";
+          }
+        }
+      }
+    } else {
+      if (!empty($_POST)) {
+        $formErrors = Verificator::checkForm($article->getForm(), $_POST);
+        if (count($formErrors) === 0) {
+          $article->setArticleInfo();
+          $registerError = $article->checkExisting('title');
+          if ($registerError === false) {
+            $article->save();
+            $success = true;
+            header("Location: /articles-list");
+          } else {
+            $formErrors[] = "Titre déjà utilisé";
+          }
         }
       }
     }
 
-    $view = new View("articleManager", "back", isset($_GET['id']) ? 'ARTICLE NAME - Edit' : 'New Article');
+    $view = new View("articleManager", "back", isset($_GET['id']) ? $article->getTitle() . ' - Edit' : 'New Article');
     $view->assign('article', $article);
     $view->assign('errors', $formErrors);
     $view->assign('success', $success);
