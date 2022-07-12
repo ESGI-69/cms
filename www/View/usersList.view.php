2c886ce14b4607b0c74fbb7ea2ab7251f63fc7a9
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-07-12 16:56:05 +0200

    feat(58-gestion-des-utilsateurs): fix selected in sidebar + media value in form

--- www/View/usersList.view.php
+++ www/View/usersList.view.php
@@ -6,6 +6,7 @@
   <div>
     <span><?= $user->firstname ?> | </span>
     <span><?= $user->email ?> | </span>
+    <span><?= $user->role ?> | </span>
     <a href="/user-manager?id=<?= $user->id ?>" class="button button--primary">Editer</a>
     <a href="/users-list?deletedId=<?= $user->id ?>" class="button button--danger">Supprimer</a>
   </div>
