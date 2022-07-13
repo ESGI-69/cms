9fe17bf6a92610623e45fb58fe6f1a4ff98f999b
Mr_Pink_Cat <gatien.anizan@gmail.com>
2022-07-13 19:29:07 +0200

    feat(media): add logger

--- www/Controller/Media.class.php
+++ www/Controller/Media.class.php
@@ -6,6 +6,7 @@ use App\Core\CleanWords;
 use App\Core\Sql;
 use App\Core\Verificator;
 use App\Core\View;
+use App\Core\Logger;
 use App\Core\AuthManager;
 
 use App\Model\Media as MediaModel;
@@ -14,10 +15,13 @@ class Media extends Sql
 {
   public function mediasList()
   {
+    $log = new Logger();
     $media = new MediaModel();
 
     if (isset($_GET['deletedId'])) {
+      $media->getMediaInfo($_GET['deletedId']);
       unlink($media->get($_GET['deletedId'])->path);
+      $log->add("media", "Media '" . $media->getName() . "' deleted by user n." . $media->getUserId() . "!");
       $media->delete($_GET['deletedId']);
     }
 
@@ -30,27 +34,23 @@ class Media extends Sql
 
   public function mediaManager()
   {
+    $log = new Logger();
     $media = new MediaModel();
     $saved = false;
     $formErrors = [];
     $registerError = false;
-
-    // à faire
-    if (isset($_GET['id'])) {
-      // $this->editMedia($_GET['editId']);
-    } else {
-      if (!empty($_POST)) {
-        $formErrors = Verificator::checkForm($media->getMediaForm(), $_POST);
-        if (count($formErrors) === 0) {
-          $media->setMediaInfo();
-          $registerError = $media->checkExisting('name');
-          if ($registerError === false) {
-            $media->saveMedia();
-            $saved = true;
-            header("Location: /medias-list");
-          } else {
-            $formErrors[] = "Nom de média déjà utilisé";
-          }
+    if (!empty($_POST)) {
+      $formErrors = Verificator::checkForm($media->getMediaForm(), $_POST);
+      if (count($formErrors) === 0) {
+        $media->setMediaInfo();
+        $registerError = $media->checkExisting('name');
+        if ($registerError === false) {
+          $media->saveMedia();
+          $saved = true;
+          $log->add("media", "Media '" . $media->getName() . "' added by user n." . $media->getUserId() . "!");
+          header("Location: /medias-list");
+        } else {
+          $formErrors[] = "Nom de média déjà utilisé";
         }
       }
     }
