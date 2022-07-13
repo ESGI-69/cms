f808c69c2d81e262137a86530179e537351b90cb
Mr_Pink_Cat <gatien.anizan@gmail.com>
2022-07-13 20:26:53 +0200

    feat(lists): add table

--- www/View/articlesList.view.php
+++ www/View/articlesList.view.php
@@ -2,10 +2,14 @@
   <a class="button button--primary button--big" href="/article-manager">New article</a>
 </div>
 
-<?php foreach ($articles as $article) : ?>
-  <div>
-    <p><?= $article->title ?></p>
-    <a href="/article-manager?id=<?= $article->id ?>" class="button button--primary">Editer</a>
-    <a href="/articles-list?deletedId=<?= $article->id ?>" class="button button--danger">Supprimer</a>
-  </div>
-<?php endforeach; ?>
+<?php $data = [
+  'config' => [
+    'editButton' => true,
+    'deleteButton' => true,
+    'editUrl' => '/article-manager',
+    'deleteUrl' => '/articles-list',
+  ],
+  'data' => $articles,
+]; ?>
+
+<?php $this->includePartial("table", $data); ?>
--- www/View/pagesList.view.php
+++ www/View/pagesList.view.php
@@ -2,10 +2,14 @@
   <a class="button button--primary button--big" href="/page-manager">New page</a>
 </div>
 
-<?php foreach ($pages as $page) : ?>
-  <div>
-    <p><?= $page->title ?></p>
-    <a href="/page-manager?id=<?= $page->id ?>" class="button button--primary">Editer</a>
-    <a href="/pages-list?deletedId=<?= $page->id ?>" class="button button--danger">Supprimer</a>
-  </div>
-<?php endforeach; ?>
+<?php $data = [
+  'config' => [
+    'editButton' => true,
+    'deleteButton' => true,
+    'editUrl' => '/page-manager',
+    'deleteUrl' => '/pages-list',
+  ],
+  'data' => $pages,
+]; ?>
+
+<?php $this->includePartial("table", $data); ?>
