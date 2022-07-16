da833646d79a069fcb5d0d6580c44a9aa1c751c8
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-07-16 19:43:02 +0200

    feat(74-pages-front): add sitemap to footer

--- www/Components/footer.component.php
+++ www/Components/footer.component.php
@@ -2,4 +2,5 @@
   <?php foreach ($footer as $footerElement) : ?>
     <a class="front-footer__item" href="/page?id=<?php echo $footerElement->id; ?>"><?php echo $footerElement->title; ?></a>
   <?php endforeach; ?>
+  <a class="front-footer__item" href="/sitemap.xml">Sitemap</a>
 </footer>
