04b4cb556cdf3ad4d0e85f92af13306e42ce6894
Mr_Pink_Cat <gatien.anizan@gmail.com>
2022-07-16 19:37:15 +0200

    fix(sidebar): change 2000 js to ES6

--- www/View/front.tpl.php
+++ www/View/front.tpl.php
@@ -39,7 +39,7 @@
         <a class="button button--primary button--big" href="./login">Se connecter</a>
         <a class="button button--primary button--big" href="./register">S'inscrire</a>
       <?php endif; ?>
-      <button href="#" class="button button--primary button--big button-icon openbtn" onclick="openNav()">
+      <button href="#" class="button button--primary button--big button-icon openbtn" id="burgerMenu">
         <svg height="18" widht="18" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="white" stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
