bc9ece93cc1f2f1be479847d7925a52ed0f6beda
Mr_Pink_Cat <gatien.anizan@gmail.com>
2022-07-16 17:30:18 +0200

    feat(sitemap): add sitemap.xml

---
+++ www/View/sitemap.view.php
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<!-- List des articles -->
+<?php foreach ($articles as $article): ?>
+  <url>
+    <loc>http://<?= $_SERVER['SERVER_NAME'] ?>/article&amp;=<?= $article->id ?></loc>
+    <lastmod><?= $article->updatedAt ?? $article->createdAt ?></lastmod>
+    <changefreq>daily</changefreq>
+  </url>
+<?php endforeach; ?>
+<!-- List des pages -->
+<?php foreach ($pages as $page): ?>
+  <url>
+    <loc>http://<?= $_SERVER['SERVER_NAME'] ?>/page&amp;=<?= $page->id ?></loc>
+    <lastmod><?= $page->updatedAt ?? $page->createdAt ?></lastmod>
+    <changefreq>monthly</changefreq>
+  </url>
+<?php endforeach; ?>
+<!-- List des pages statics -->
+<?php foreach ($staticUrls as $url => $config): ?>
+<?php if (
+  $url !== "/sitemap.xml"
+  && $url !== "/article"
+  && $url !== "/page"
+  && !isset($config['security'])
+) : ?>
+  <url>
+    <loc>http://<?= $_SERVER['SERVER_NAME'] ?><?= $url ?></loc>
+  </url>
+<?php endif; ?>
+<?php endforeach; ?>
+</urlset>
