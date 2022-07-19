cf890d88512f7b36cef3d057d3aed456b2c54dbb
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-07-19 13:39:36 +0200

    feat(102-modifs-globals): add alt to img

--- www/View/me.view.php
+++ www/View/me.view.php
@@ -2,5 +2,5 @@
 if ($isAuth === true) {
   $this->includePartial("form", $user->getUserFormFront());
 } else {
-  die('404 Not found<br><img src="https://http.cat/404" />');
+  die('404 Not found<br><img src="https://http.cat/404" alt="cat 404 http error" />');
 }
