915ef85f5463fcdf3c4b10d54a30a8fd0652b047
Quentin Peltier <quentin.peltier.pro@gmail.com>
2022-07-19 11:22:58 +0200

    feat(modifs-globals): if db wikiki not set create one and redirect on install when no tables

--- www/Core/DbConnection.class.php
+++ www/Core/DbConnection.class.php
@@ -6,6 +6,7 @@ class DbConnection
 {
   private static $instance = null;
   public $pdo;
+  private $pdoCreate;
 
   // a faire dans le construc de Sql
   // $this->pdo = DbConnection::getInstance()->pdo;
@@ -14,11 +15,27 @@ class DbConnection
   private function __construct()
   {
     //Se connecter à la bdd
-    try{
-      $this->pdo = new \PDO( DBDRIVER.":host=".DBHOST.";port=".DBPORT.";dbname=".DBNAME
-        ,DBUSER, DBPWD , [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_WARNING]);
-    }catch (\Exception $e){
-      die("Erreur SQL : ".$e->getMessage());
+    try {
+      $this->pdo = new \PDO(
+        DBDRIVER . ":host=" . DBHOST . ";port=" . DBPORT . ";dbname=" . DBNAME,
+        DBUSER,
+        DBPWD,
+        [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_WARNING]
+      );
+    } catch (\Exception $e) {
+      if ($e->getMessage() === "SQLSTATE[HY000] [1049] Unknown database 'wikiki'") {
+        $this->pdoCreate = new \PDO(
+          DBDRIVER . ":host=" . DBHOST . ";port=" . DBPORT,
+          DBUSER,
+          DBPWD,
+          [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_WARNING]
+        );
+        $this->pdoCreate->exec("CREATE DATABASE wikiki");
+        $this->pdoCreate = null;
+        header('Location: /');
+      } else {
+        die("Erreur SQL : " . $e->getMessage());
+      }
     }
   }
 
@@ -39,13 +56,13 @@ class DbConnection
       }
       return self::$instance;
     }
-  */
+   */
 
-  public static function getInstance() {
+  public static function getInstance()
+  {
     if (is_null(self::$instance)) {
       self::$instance = new DbConnection();
     }
     return self::$instance;
   }
-
 }
